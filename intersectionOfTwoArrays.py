250896c33c3129dcdd91c17f0a0b7c1c7ed7c937
herbjrob <43552696+herbjrob@users.noreply.github.com>
2020-05-27 01:55:39 -0400

    Completed Intersection of two arrays

---
+++ intersectionOfTwoArrays.py
@@ -0,0 +1,14 @@
+ans = []
+        
+i = 0
+j = 0
+        
+while i < len(nums1) and j < len(nums2):
+            if nums1[i] == nums2[j]:
+                ans.append(nums1[i])
+                i += 1
+                j += 1
+            elif nums1[i] > nums2[j]:
+                j += 1
+            else:
+                i += 1
